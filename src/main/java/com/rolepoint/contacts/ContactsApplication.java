39679d5dc3a00343a954580c1f889267c7f29dd2
Theodore Sentelidis <theodore.sentelidis@paconsulting.com>
2019-03-11 16:55:49 +0000

    return person list

--- src/main/java/com/rolepoint/contacts/ContactsApplication.java
+++ src/main/java/com/rolepoint/contacts/ContactsApplication.java
@@ -3,11 +3,12 @@ package com.rolepoint.contacts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ContactsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactsApplication.class, args);
 	}
-
 }
--- src/main/java/com/rolepoint/contacts/Person.java
+++ src/main/java/com/rolepoint/contacts/Person.java
@@ -1,13 +1,17 @@
 package com.rolepoint.contacts;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class Person {
+    private List<String> job_history;
+    private String company;
+    private String email;
+    private String city;
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String country;
 }
